b73331fae2a5f333a6be28f2f41adcb247ac257d
naoya.nakamura <risuo@naoyanakamura-no-MacBook-Air.local>
2018-08-06 08:45:30 +0900

    refactor sklearn models

--- akebono/models/__init__.py
+++ akebono/models/__init__.py
@@ -1,7 +1,9 @@
 from akebono.logging import getLogger
 from .base import WrappedModel
+from .sklearn import get_wrapped_sklearn_model
 from akebono.io.operation.dumper import dump_sklearn_model
 import os
+import re
 import copy
 
 logger = getLogger(__name__)
@@ -29,50 +31,6 @@ class WrappedLGBMClassifier(WrappedModel):
     dump = dump_sklearn_model
     
 
-class WrappedRandomForestClassifier(WrappedModel):
-    def base_init_finished(self):
-        if not self._is_rebuild:
-            self.reset()
-    
-    def fit(self, X, y):
-        self._value.fit(X, y, **self._fit_kwargs)
-        return self
-    
-    def reset(self):
-        from sklearn.ensemble import RandomForestClassifier
-        self._value = RandomForestClassifier(**self._init_kwargs)
-    
-    def predict(self, X):
-        return self._value.predict(X)
-    
-    def predict_proba(self, X):
-        return self._value.predict_proba(X)
-     
-    dump = dump_sklearn_model
-
-
-class WrappedLogisticRegression(WrappedModel):
-    def base_init_finished(self):
-        if not self._is_rebuild:
-            self.reset()
-    
-    def fit(self, X, y):
-        self._value.fit(X, y, **self._fit_kwargs)
-        return self
-    
-    def reset(self):
-        from sklearn.linear_model import LogisticRegression
-        self._value = LogisticRegression(**self._init_kwargs)
-    
-    def predict(self, X):
-        return self._value.predict(X)
-    
-    def predict_proba(self, X):
-        return self._value.predict_proba(X)
-     
-    dump = dump_sklearn_model
-    
-
 def get_model(model_config):
     if not isinstance(model_config, dict):
         raise TypeError('model_config must be dict.')
@@ -82,11 +40,9 @@ def get_model(model_config):
     model_name = mcc.pop('name')
     mcc['is_rebuild'] = False
 
-    if model_name == 'lgbm_classifier':
+    if re.search('^Sklearn.+$', model_name) is not None:
+        return get_wrapped_sklearn_model(model_name)(**mcc)
+    elif model_name == 'LGBMClassifier':
         return WrappedLGBMClassifier(**mcc)
-    elif model_name == 'logistic_regression':
-        return WrappedLogisticRegression(**mcc)
-    elif model_name == 'random_forest_classifier':
-        return WrappedRandomForestClassifier(**mcc)
     else:
         raise Exception('{} does not found.'.format(model_name))
---
+++ akebono/models/sklearn.py
@@ -0,0 +1,64 @@
+import re
+
+from .base import WrappedModel
+from akebono.io.operation.dumper import dump_sklearn_model
+from akebono.utils import load_object_by_str
+from akebono.logging import getLogger
+
+
+logger = getLogger(__name__)
+
+
+_valid_models = {
+    'LogisticRegression': 'sklearn.linear_model',
+    'RandomForestClassifier': 'sklearn.ensemble',
+}
+
+
+def _fit(model, X, y):
+    model._value.fit(X, y, **model._fit_kwargs)
+    return model
+
+
+def _predict(model, X):
+    return model._value.predict(X)
+
+
+def _predict_proba(model, X):
+    return model._value.predict_proba(X)
+
+
+def _generate_reset_func(cls):
+
+    def _func(model):
+        model._value = cls(**model._init_kwargs)
+    return _func
+
+
+def _base_init_finished(model):
+    if not model._is_rebuild:
+        model.reset()
+
+
+def get_wrapped_sklearn_model(model_cls_str):
+    # model_cls_str は'^Sklearn.+$' にマッチする前提
+    model_cls_str = re.sub('Sklearn', '', model_cls_str)
+    model_cls_mod = _valid_models.get(model_cls_str)
+    if model_cls_mod is None:
+        raise Exception('{} is invalid.'.format(model_cls_str))
+    model_cls = load_object_by_str('{}@{}'.format(model_cls_str, model_cls_mod))
+
+    cls_attrs = {
+        'fit': _fit,
+        'reset': _generate_reset_func(model_cls),
+        'dump': dump_sklearn_model,
+        'base_init_finished': _base_init_finished,
+    }
+
+    if hasattr(model_cls, 'predict'):
+        cls_attrs['predict'] = _predict
+    if hasattr(model_cls, 'predict_proba'):
+        cls_attrs['predict_proba'] = _predict_proba
+    logger.debug('model_cls_str: {} .. created.'.format(model_cls_str))
+
+    return type('WrappedSklearn{}'.format(model_cls_str), (WrappedModel,), cls_attrs)
